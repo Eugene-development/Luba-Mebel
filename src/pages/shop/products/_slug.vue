4384ed462bd8e1e8e43909326fb1d5726df15a55
indexpro24@gmail.com <indexpro24@gmail.com>
2021-06-22 15:05:40 +0300

    git init

--- src/pages/shop/products/_slug.vue
+++ src/pages/shop/products/_slug.vue
@@ -86,6 +86,57 @@
       </ul>
 
     </div>
+
+
+    <div class="relative py-16 bg-white overflow-hidden">
+      <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+        <div class="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+          <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+            <defs>
+              <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+              </pattern>
+            </defs>
+            <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+          </svg>
+          <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+            <defs>
+              <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+              </pattern>
+            </defs>
+            <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+          </svg>
+          <!--        <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">-->
+          <!--          <defs>-->
+          <!--            <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">-->
+          <!--              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />-->
+          <!--            </pattern>-->
+          <!--          </defs>-->
+          <!--          <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />-->
+          <!--        </svg>-->
+        </div>
+      </div>
+
+      <div class="relative px-4 sm:px-6 lg:px-8">
+        <div class="text-lg max-w-prose mx-auto">
+          <h1>
+            <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-main sm:text-4xl">Какой то текст</span>
+          </h1>
+          <p class="mt-8 text-xl text-gray-500 leading-8">Здесть размещать текста для продвижения сайта</p>
+          <p class="mt-8 text-xl text-gray-500 leading-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ex fugiat itaque molestias mollitia neque ratione voluptatem. Adipisci, asperiores consectetur cupiditate dolor eaque facere facilis nemo nostrum quaerat quia quisquam recusandae similique tempora? Alias aliquid aperiam aspernatur, atque commodi consequuntur earum esse eum laudantium magnam, nam neque nostrum possimus provident quas quisquam, quo repellat ullam veniam voluptas. Accusantium adipisci aliquam animi assumenda consectetur debitis dignissimos dolorem doloremque eos eum facilis fugiat harum illum incidunt inventore ipsam iste labore laudantium libero modi molestiae nostrum, odit officia officiis perspiciatis placeat praesentium provident qui quia quibusdam quos rem reprehenderit rerum sapiente sint veniam vitae! Accusamus aperiam dicta dignissimos dolorum ea eligendi error eveniet facere ipsum itaque iure laborum maxime odit, officia officiis pariatur provident quam quo rem repellat reprehenderit repudiandae similique soluta, sunt ullam unde velit vitae! Iure maxime minus placeat repudiandae similique. Ad blanditiis consequuntur doloremque ea error expedita, illo itaque laboriosam nulla quam ratione repellat vel veritatis, vitae voluptate? A, accusamus eius ex excepturi illo molestiae neque numquam obcaecati odio possimus quaerat repellat reprehenderit unde. A accusamus adipisci aliquam aperiam, aspernatur consequatur dolorem ex exercitationem fugiat harum incidunt ipsa maxime minima molestias perferendis suscipit, totam. Cupiditate inventore laborum odio recusandae sint!</p>
+        </div>
+        <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+          <!--        <h3>Ждём вас!!!</h3>-->
+          <figure>
+            <!--          <img class="w-full rounded-lg" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3" alt="" width="1310" height="873">-->
+          </figure>
+          <!--        <p>Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>. Amet, massa quam varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie. Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum convallis quam.</p>-->
+          <!--        <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>-->
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
