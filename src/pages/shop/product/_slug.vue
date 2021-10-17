e5ce6c4a6c34dfce1997ce8b726fea02796390ba
indexpro24@gmail.com <indexpro24@gmail.com>
2021-10-17 13:49:16 +0300

    git init

--- src/pages/shop/product/_slug.vue
+++ src/pages/shop/product/_slug.vue
@@ -54,12 +54,10 @@
             <!-- Product info -->
             <div class="mt-10 px-4 sm:px-0 sm:mt-16 lg:mt-0">
               <h2 class="text-sm title-font text-gray-500 tracking-widest">
-                <NuxtLink :to="'/shop/products/' + product[0].slug">
+                <NuxtLink :to="'/shop/products/' + product[0].category.slug">
                   <span class="ml-auto text-gray-900">
-                  {{ product[0].name }}
+                  {{ product[0].category.name }}
                 </span>
-                  <!--                TODO без цикла если?-->
-
                 </NuxtLink>
               </h2>
 
@@ -71,45 +69,45 @@
               </div>
 
               <!-- Reviews -->
-<!--              <div class="mt-3">-->
-<!--                <h3 class="sr-only">Reviews</h3>-->
-<!--                <div class="flex items-center">-->
-<!--                  <div class="flex items-center">-->
-<!--                    &lt;!&ndash;-->
-<!--                      Heroicon name: solid/star-->
+              <div class="mt-3">
+                <h3 class="sr-only">Reviews</h3>
+                <div class="flex items-center">
+                  <div class="flex items-center">
+                    <!--
+                      Heroicon name: solid/star
 
-<!--                      Active: "text-indigo-500", Inactive: "text-gray-300"-->
-<!--                    &ndash;&gt;-->
-<!--                    <svg class="h-5 w-5 flex-shrink-0 text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">-->
-<!--                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />-->
-<!--                    </svg>-->
+                      Active: "text-main", Inactive: "text-gray-300"
+                    -->
+                    <svg class="h-5 w-5 flex-shrink-0 text-red-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
 
-<!--                    &lt;!&ndash; Heroicon name: solid/star &ndash;&gt;-->
-<!--                    <svg class="h-5 w-5 flex-shrink-0 text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">-->
-<!--                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />-->
-<!--                    </svg>-->
+                    <!-- Heroicon name: solid/star -->
+                    <svg class="h-5 w-5 flex-shrink-0 text-red-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
 
-<!--                    &lt;!&ndash; Heroicon name: solid/star &ndash;&gt;-->
-<!--                    <svg class="h-5 w-5 flex-shrink-0 text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">-->
-<!--                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />-->
-<!--                    </svg>-->
+                    <!-- Heroicon name: solid/star -->
+                    <svg class="h-5 w-5 flex-shrink-0 text-red-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
 
-<!--                    &lt;!&ndash; Heroicon name: solid/star &ndash;&gt;-->
-<!--                    <svg class="h-5 w-5 flex-shrink-0 text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">-->
-<!--                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />-->
-<!--                    </svg>-->
+                    <!-- Heroicon name: solid/star -->
+                    <svg class="h-5 w-5 flex-shrink-0 text-red-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
 
-<!--                    &lt;!&ndash; Heroicon name: solid/star &ndash;&gt;-->
-<!--                    <svg class="h-5 w-5 flex-shrink-0 text-gray-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">-->
-<!--                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />-->
-<!--                    </svg>-->
-<!--                  </div>-->
-<!--                  <p class="sr-only">4 out of 5 stars</p>-->
-<!--                </div>-->
-<!--              </div>-->
+                    <!-- Heroicon name: solid/star -->
+                    <svg class="h-5 w-5 flex-shrink-0 text-red-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+
+                  </div>
+                </div>
+              </div>
 
               <div class="mt-6">
-                <h3 class="sr-only">Description</h3>
+                <h3 class="sr-only">Описание</h3>
 
                 <div class="flex mb-4">
                   <button
@@ -245,60 +243,60 @@
                 </div>
               </form>
 
-              <section aria-labelledby="details-heading" class="mt-12">
-                <h2 id="details-heading" class="sr-only">Additional details</h2>
+<!--              <section aria-labelledby="details-heading" class="mt-12">-->
+<!--                <h2 id="details-heading" class="sr-only">Additional details</h2>-->
 
-                <div class="border-t divide-y divide-gray-200">
-                  <div>
-                    <h3>
-                      <!-- Expand/collapse question button -->
-                      <button type="button" class="group relative w-full py-6 flex justify-between items-center text-left" aria-controls="disclosure-1" aria-expanded="false">
-                        <!-- Open: "text-indigo-600", Closed: "text-gray-900" -->
-                        <span class="text-gray-900 text-sm font-medium">
-                    Features
-                  </span>
-                        <span class="ml-6 flex items-center">
-                    <!--
-                      Heroicon name: outline/plus-sm
+<!--                <div class="border-t divide-y divide-gray-200">-->
+<!--                  <div>-->
+<!--                    <h3>-->
+<!--                      &lt;!&ndash; Expand/collapse question button &ndash;&gt;-->
+<!--                      <button type="button" class="group relative w-full py-6 flex justify-between items-center text-left" aria-controls="disclosure-1" aria-expanded="false">-->
+<!--                        &lt;!&ndash; Open: "text-indigo-600", Closed: "text-gray-900" &ndash;&gt;-->
+<!--                        <span class="text-gray-900 text-sm font-medium">-->
+<!--                    Features-->
+<!--                  </span>-->
+<!--                        <span class="ml-6 flex items-center">-->
+<!--                    &lt;!&ndash;-->
+<!--                      Heroicon name: outline/plus-sm-->
 
-                      Open: "hidden", Closed: "block"
-                    -->
-                    <svg class="block h-6 w-6 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                    </svg>
-                          <!--
-                            Heroicon name: outline/minus-sm
+<!--                      Open: "hidden", Closed: "block"-->
+<!--                    &ndash;&gt;-->
+<!--                    <svg class="block h-6 w-6 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">-->
+<!--                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />-->
+<!--                    </svg>-->
+<!--                          &lt;!&ndash;-->
+<!--                            Heroicon name: outline/minus-sm-->
 
-                            Open: "block", Closed: "hidden"
-                          -->
-                    <svg class="hidden h-6 w-6 text-indigo-400 group-hover:text-indigo-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
-                    </svg>
-                  </span>
-                      </button>
-                    </h3>
-                    <div class="pb-6 prose prose-sm" id="disclosure-1">
-                      <ul role="list">
-                        <li>Multiple strap configurations</li>
+<!--                            Open: "block", Closed: "hidden"-->
+<!--                          &ndash;&gt;-->
+<!--                    <svg class="hidden h-6 w-6 text-indigo-400 group-hover:text-main" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">-->
+<!--                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />-->
+<!--                    </svg>-->
+<!--                  </span>-->
+<!--                      </button>-->
+<!--                    </h3>-->
+<!--                    <div class="pb-6 prose prose-sm" id="disclosure-1">-->
+<!--                      <ul role="list">-->
+<!--                        <li>Multiple strap configurations</li>-->
 
-                        <li>Spacious interior with top zip</li>
+<!--                        <li>Spacious interior with top zip</li>-->
 
-                        <li>Leather handle and tabs</li>
+<!--                        <li>Leather handle and tabs</li>-->
 
-                        <li>Interior dividers</li>
+<!--                        <li>Interior dividers</li>-->
 
-                        <li>Stainless strap loops</li>
+<!--                        <li>Stainless strap loops</li>-->
 
-                        <li>Double stitched construction</li>
+<!--                        <li>Double stitched construction</li>-->
 
-                        <li>Water-resistant</li>
-                      </ul>
-                    </div>
-                  </div>
+<!--                        <li>Water-resistant</li>-->
+<!--                      </ul>-->
+<!--                    </div>-->
+<!--                  </div>-->
 
-                  <!-- More sections... -->
-                </div>
-              </section>
+<!--                  &lt;!&ndash; More sections... &ndash;&gt;-->
+<!--                </div>-->
+<!--              </section>-->
             </div>
           </div>
         </div>
