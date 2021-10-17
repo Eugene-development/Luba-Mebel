5c4a1d6d851794fbe6c23bf90f8ff3d5d154a4e5
indexpro24@gmail.com <indexpro24@gmail.com>
2021-10-17 20:24:55 +0300

    git init

--- src/pages/shop/product/_slug.vue
+++ src/pages/shop/product/_slug.vue
@@ -65,7 +65,7 @@
 
               <div class="mt-3">
                 <h2 class="sr-only">Product information</h2>
-<!--                <p class="text-3xl text-gray-900">{{ product[0].price }} руб/{{ product[0].unit }}</p>-->
+                <p class="text-3xl text-gray-900">{{ product[0].size[0].price.price }} руб/{{ product[0].unit }}</p>
               </div>
 
               <!-- Reviews -->
@@ -150,9 +150,9 @@
                       <div class="mt-1 relative">
                         <button type="button" class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" aria-haspopup="listbox" aria-expanded="true" aria-labelledby="listbox-label">
                         <span class="block truncate">
-                          Tom Cook
+                          {{ product[0].size[0].size }}-{{ product[0].size[0].id }}
                         </span>
-                                            <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                          <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                           <!-- Heroicon name: solid/selector -->
                           <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                             <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -176,10 +176,11 @@
 
                             Highlighted: "text-white bg-indigo-600", Not Highlighted: "text-gray-900"
                           -->
-                          <li class="text-gray-900 cursor-default select-none relative py-2 pl-8 pr-4" id="listbox-option-0" role="option">
+                          <li v-for="(size, idx) of product[0].size" :key="size.id"
+                              class="text-gray-900 cursor-default select-none relative py-2 pl-8 pr-4" id="listbox-option-0" role="option">
                             <!-- Selected: "font-semibold", Not Selected: "font-normal" -->
                             <span class="font-normal block truncate">
-                              Wade Cooper
+                              {{ size.size }}
                             </span>
 
                             <!--
