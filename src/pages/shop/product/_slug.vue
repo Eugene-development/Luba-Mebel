fb3615beb94b803c16cf307db6fae14cad942948
indexpro24@gmail.com <indexpro24@gmail.com>
2021-10-18 19:21:50 +0300

    git init

--- src/pages/shop/product/_slug.vue
+++ src/pages/shop/product/_slug.vue
@@ -65,6 +65,7 @@
 
               <div class="mt-3">
                 <h2 class="sr-only">Product information</h2>
+                <p class="text-3xl text-gray-900">{{ product[0].size[0].price.price }} руб/{{ product[0].unit }}</p>
                 <p class="text-3xl text-gray-900">{{ currentProduct.price }} руб/{{ product[0].unit }}</p>
               </div>
 
@@ -149,6 +150,9 @@
                     <div>
                       <div class="mt-1 relative">
                         <button type="button" class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-red-800 focus:border-red-800 sm:text-sm" aria-haspopup="listbox" aria-expanded="true" aria-labelledby="listbox-label">
+                        <span class="block truncate">
+                          {{ product[0].size[0].size }}
+                        </span>
                         <span class="block truncate">
                           {{ currentProduct.size }}
                         </span>
